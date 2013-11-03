cb4b4d24d876738cbca6fc81fe09e1e3a2fcf292
Darius <perfx@Dariuss-MacBook-Pro.local>
2013-11-03 13:55:05 +0200

    renamed to app.js.

--- js/main-ck.js
+++
@@ -1,3 +0,0 @@
-/**
- * Layout specific inititialisation
- */var layout={init:function(){$("body").removeClass("js_off");for(var e in this)/^init.+/.test(e)&&this[e]()},initForms:function(){typeof $.fn.selectBox=="function"&&$("#p select").selectBox();typeof $.fn.placeholder=="function"&&$("input[placeholder], textarea[placeholder]").placeholder()},initFancybox:function(){typeof $.fn.fancybox=="function"&&$(".fancybox").fancybox()}};$(document).ready(function(){layout.init()});
